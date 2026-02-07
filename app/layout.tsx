b0bde9727f34de9f20aa05d322c573864cbdab11
Aleksandar Shokolarov <akshokolarov@gmail.com>
2026-02-07 01:34:40 +0100

    favicons added

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,12 @@ export default function RootLayout({
                     href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;600;700&display=swap"
                     rel="stylesheet"
                 />
-            </head>
-            <head>
-                <link rel="icon" href="/favicon.png?v=2" />
+                {/* Favicons */}
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+                <link rel="icon" type="image/png" href="/favicon.png?v=2" />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2" />
+                <meta name="theme-color" content="#ffffff" />
             </head>
             <body>
                 <Navigation />
